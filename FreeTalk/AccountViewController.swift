bdbcdbe1d57378ae264759babe3390a5070d6dae
KimJunseok <jskim2301@yahoo.co.jp>
2020-06-23 00:56:09 +0900

    AccountInfo Modify

--- FreeTalk/AccountViewController.swift
+++ FreeTalk/AccountViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 import Firebase
+import Kingfisher
 
 class AccountViewController: UIViewController {
-
+    
     @IBOutlet weak var statusMessge: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusMessageLabel: UIButton!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    let databaseRef = Database.database().reference()
+    var userModel : UserModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         statusMessge.addTarget(self, action: #selector(setStatusMessage), for: .touchUpInside)
         logoutButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
-
+        getAccountInfo()
+        
+        
         // Do any additional setup after loading the view.
     }
     
@@ -27,17 +38,31 @@ class AccountViewController: UIViewController {
         
         let alertVC = UIAlertController(title: "ステータスメッセージ", message: nil, preferredStyle: .alert)
         alertVC.addTextField { (textfield) in
-            textfield.placeholder = "ステータスメッセージを入力して下さい。"
+            
+            if (self.userModel?.statusMessage!.isEmpty)!{
+                textfield.placeholder = "ステータスメッセージを入力して下さい。"
+            }else{
+                textfield.text = self.userModel?.statusMessage
+            }
+            
         }
-        alertVC.addAction(UIAlertAction(title: "確認", style: .default, handler: { (action) in
+        let okAction = UIAlertAction(title: "確認", style: .default, handler: { (action) in
             if let textField = alertVC.textFields?.first{
                 let dic = ["statusMessage":textField.text!]
                 let uid = Auth.auth().currentUser?.uid
                 let databaseRef = Database.database().reference()
                 databaseRef.child("users").child(uid!).updateChildValues(dic)
             }
-        }))
-
+        })
+        // MARK: - Observer修正、入力したらボタン活性化に更新
+        
+        alertVC.addAction(okAction)
+        okAction.isEnabled = false
+        
+        
+        
+        
+        
         alertVC.addAction(UIAlertAction(title: "取消", style: .cancel, handler: { (action) in
             
         }))
@@ -45,33 +70,68 @@ class AccountViewController: UIViewController {
         
     }
     
+    @objc func getAccountInfo(){
+        let currentUid = Auth.auth().currentUser?.uid
+        databaseRef.child("users").child(currentUid!).observe(.value) { (datasnapshot) in
+            
+            self.userModel = UserModel()
+            let userInfo = datasnapshot.value as! [String:Any]
+            self.userModel?.setValuesForKeys(userInfo)
+            
+            guard let name = self.userModel?.userName else {
+                return
+            }
+            guard let statusMessage = self.userModel?.statusMessage else {
+                return
+            }
+            guard let imageUrl = self.userModel?.profileImageUrl else {
+                return
+            }
+            
+            let url = URL(string: imageUrl)
+            
+            self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width/2
+            self.profileImageView.clipsToBounds = true
+            self.profileImageView.kf.setImage(with: url)
+            self.nameLabel.text = name
+            if statusMessage.isEmpty{
+                self.statusMessageLabel.setTitle("ステータスメッセージ", for: .normal)
+            }else{
+                self.statusMessageLabel.setTitle(statusMessage, for: .normal)
+            }
+            
+        }
+    }
+    
     @objc func logout(){
-
+        
         do {
-            // MARK: - 再度確認必要
+            
             try Auth.auth().signOut()
             
             let s = UIStoryboard(name: "LoginViewController", bundle: nil)
             let loginVC = s.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
             self.present(loginVC, animated: true, completion: nil)
-
+            
         } catch  {
             print("error")
         }
         
     }
     
+
+    
     
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
